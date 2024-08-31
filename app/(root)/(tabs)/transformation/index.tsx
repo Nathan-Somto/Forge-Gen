f509c06c340d8b29a9f6931ba139b2ea82bccc51
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-08-31 17:15:48 +0100

    refactor: Update transformation page to handle insufficient credits

--- app/(root)/(tabs)/transformation/index.tsx
+++ app/(root)/(tabs)/transformation/index.tsx
@@ -1,4 +1,4 @@
-import { View,  FlatList, TouchableOpacity } from "react-native";
+import { View, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 import { transformationLinks } from "@/constants/Values";
 import GradientHeading from "@/components/GradientHeading";
@@ -6,30 +6,43 @@ import PrimaryBackground from "@/components/PrimaryBackground";
 import { router } from "expo-router";
 import Colors from "@/constants/Colors";
 import { Text } from "@/components/ui/Text";
+import InsufficientCreditsSheet from "@/components/InsufficientCoinsSheet";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function transformation() {
+  const [showSheet, setShowSheet] = React.useState(false);
+  const {auth: {user}} = useAuth()
+  const creditBalance = user?.creditBalance || 0
+  const handleRedirect = (type: TransformationTypeKey) => {
+    if (creditBalance < 1) {
+      setShowSheet(true);
+      return;
+    }
+    router.push(`/(root)/transformation/new/${type}`);
+  };
+  const handleClose = () => {
+    setShowSheet(false);
+  };
   return (
     <PrimaryBackground>
       <View className="flex-1 px-3 py-3 pb-5">
-      <View className="mt-4" />
+        <View className="mt-4" />
         <GradientHeading>Select Transformation Type</GradientHeading>
         <View className="mb-5" />
         <FlatList
           data={transformationLinks}
           numColumns={2}
           keyExtractor={(item) => item.type}
-          renderItem={({ item: {icon: Icon, label, type} }) => (
+          renderItem={({ item: { icon: Icon, label, type } }) => (
             <TouchableOpacity
-              onPress={() =>
-                router.push(`/(root)/transformation/new/${type}`)
-              }
+              onPress={() => handleRedirect(type)}
               className="h-[200px] w-[47%] m-2 px-2 justify-center items-center rounded-2xl"
               style={{ backgroundColor: Colors.secondary }}
             >
-              <View>{<Icon/>}</View>
+              <View>{<Icon />}</View>
               <Text
                 className="text-center opacit-80 w-[80%] mx-auto mt-2"
-                style={{ color: Colors.neutral}}
+                style={{ color: Colors.neutral }}
               >
                 {label}
               </Text>
@@ -37,6 +50,10 @@ export default function transformation() {
           )}
         />
       </View>
+      <InsufficientCreditsSheet 
+      showSheet={showSheet} 
+      onClose={handleClose} 
+      />
     </PrimaryBackground>
   );
 }
--- app/(root)/(tabs)/transformation/new/[type].tsx
+++ app/(root)/(tabs)/transformation/new/[type].tsx
@@ -1,16 +1,19 @@
-import { ScrollView, View} from 'react-native'
-import React from 'react'
-import PrimaryBackground from '@/components/PrimaryBackground';
-import { Text } from '@/components/ui/Text';
-import TransformationForm from '@/components/TransformationForm';
-import { useLocalSearchParams } from 'expo-router';
+import { ScrollView, View } from "react-native";
+import React from "react";
+import PrimaryBackground from "@/components/PrimaryBackground";
+import { Text } from "@/components/ui/Text";
+import TransformationForm from "@/components/TransformationForm";
+import { useLocalSearchParams } from "expo-router";
 export default function NewTransformation() {
- const {type} = useLocalSearchParams<{type:string}>();
+  const { type } = useLocalSearchParams<{ type: string }>();
   return (
     <PrimaryBackground>
-      <ScrollView className='px-5 flex-1'>
-     <TransformationForm type={type as TransformationTypeKey} action='Add'/>
+      <ScrollView className="px-5 flex-1">
+        <TransformationForm
+          type={type as TransformationTypeKey}
+          userId="1234"
+        />
       </ScrollView>
     </PrimaryBackground>
-  )
+  );
 }
